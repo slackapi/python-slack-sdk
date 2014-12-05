0609f095d1e4d6f1c9d1436356313b129a0ad428
Ryan Huber <rhuber@gmail.com>
2014-12-04 16:38:23 -0800

    have the proxy object do more things

--- slackclient/_channel.py
+++ slackclient/_channel.py
@@ -4,18 +4,22 @@ class Channel(object):
         self.name = name
         self.id = id
         self.members = members
+
     def __eq__(self, compare_str):
         if self.name == compare_str or self.id == compare_str:
             return True
         else:
             return False
+
     def __str__(self):
         data = ""
         for key in self.__dict__.keys():
             data += "{} : {}\n".format(key, str(self.__dict__[key])[:40])
         return data
+
     def __repr__(self):
         return self.__str__()
+
     def send_message(self, message):
         message_json = {"type": "message", "channel": self.id, "text": message}
         self.server.send_to_websocket(message_json)
