f892d66e55bad8fa4f22a695325203ce961f55ce
Christo Goosen <christo.goosen@takectrl.co.za>
2014-12-08 15:39:40 -0800

    Version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='slackclient',
-      version='0.1',
+      version='0.11',
       description='Python client for Slack.com',
       url='http://github.com/slackhq/python-slackclient',
       author='Ryan Huber',
