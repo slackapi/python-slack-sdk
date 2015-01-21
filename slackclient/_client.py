19097fc915bb1022165efab269c0f22270f23de7
broamski <brian.nuszkowski@me.com>
2015-01-21 17:42:46 -0500

    If the provided channel name contains a # character, remove it.

--- slackclient/_client.py
+++ slackclient/_client.py
@@ -33,6 +33,7 @@ class SlackClient(object):
             raise SlackNotConnected
 
     def rtm_send_message(self, channel, message):
+        channel = channel.replace('#', '')
         return self.server.channels.find(channel).send_message(message)
 
 class SlackNotConnected(Exception):
