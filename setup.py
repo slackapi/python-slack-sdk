195e16a1a77059e5db81d7f6832ecd48f631a927
Christo Goosen <christo.goosen@takectrl.co.za>
2015-02-13 10:54:13 -0800

    fix dep for websocket-client

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
 setup(name='slackclient',
-      version='0.13',
+      version='0.14',
       description='Python client for Slack.com',
       url='http://github.com/slackhq/python-slackclient',
       author='Ryan Huber',
       author_email='ryan@slack-corp.com',
       license='MIT',
       packages=['slackclient'],
+      install_requires=[
+        'websocket-client',
+      ],
       zip_safe=False)
