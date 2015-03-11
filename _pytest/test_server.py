e9a8aecdc55ddbfd40daf7fe4a8ef92e788f6d8d
Ryan Huber <rhuber@gmail.com>
2015-03-11 13:39:46 -0700

    adding tests..

--- _pytest/test_server.py
+++ _pytest/test_server.py
@@ -1,5 +1,5 @@
 from slackclient._user import User
-from slackclient._server import Server
+from slackclient._server import Server, SlackLoginError
 from slackclient._channel import Channel
 import json
 import pytest
@@ -21,3 +21,13 @@ def test_Server_parse_channel_data(server, login_data):
 def test_Server_parse_user_data(server, login_data):
     server.parse_user_data(login_data["users"])
     assert type(server.users.find('fakeuser')) == User
+
+def test_Server_cantconnect(server):
+    with pytest.raises(SlackLoginError):
+        reply = server.ping()
+
+@pytest.mark.xfail
+def test_Server_ping(server, monkeypatch):
+    #monkeypatch.setattr("", lambda: True)
+    monkeypatch.setattr("websocket.create_connection", lambda: True)
+    reply = server.ping()
