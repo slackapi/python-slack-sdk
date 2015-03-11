bb9a45d982e80b7ae1dd6f5de312fb980d714bb1
Christo Goosen <christo.goosen@takectrl.co.za>
2015-03-11 15:41:58 -0700

    add slackclient tests

--- _pytest/conftest.py
+++ _pytest/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from slackclient._channel import Channel
 from slackclient._server import Server
+from slackclient._client import SlackClient
 
 @pytest.fixture
 def server(monkeypatch):
@@ -8,6 +9,12 @@ def server(monkeypatch):
     return myserver
 
 @pytest.fixture
+def slackclient(server):
+    myslackclient = SlackClient('xoxp-1234123412341234-12341234-1234')
+    return myslackclient
+
+@pytest.fixture
 def channel(server):
     mychannel = Channel(server, "somechannel", "C12341234", ["user"])
     return mychannel
+
