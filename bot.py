6a64bf409077d1ea0c903f0d554c4c95cb34e48d
Jacquin Théo <theo.jacquin@telecom-bretagne.eu>
2015-10-13 18:11:25 +0200

    Changement de truc sale.

--- bot.py
+++ bot.py
@@ -9,19 +9,12 @@ sc = SlackClient(token)
 
 listsOfChans = json.loads(sc.api_call("channels.list"))
 
+chans = {str(listsOfChans[u'channels'][i][u'name']):str(listsOfChans[u'channels'][i][u'id']) for i in range(len(listsOfChans[u'channels']))}
 
-nameOfChans = [str(listsOfChans[u'channels'][i][u'name']) for i in range(len(listsOfChans[u'channels']))]
-idOfChans = [str(listsOfChans[u'channels'][i][u'id']) for i in range(len(listsOfChans[u'channels']))]
 
-idOfGeneral=''
-for i in range(len(nameOfChans)):
-    if nameOfChans[i] == "general":
-        idOfGeneral = idOfChans[i]
 
-print idOfChans
-print nameOfChans
-print idOfGeneral
-#resp1 = sc.api_call("channels.join", name=idOfGeneral)
-#print(resp1)
-resp = sc.api_call("chat.postMessage", as_user="true", channel=idOfGeneral, text="Demain midi, en entrée une superbe salade de chèvre chaud - qui, si je peux me permettre, est sa mère la pute bonne - En plat du thon à la catalane avec son riz au safran. Nous vous proposerons un crumble en dessert chaud.\nDemain soir des aiguillettes de poulet avec des pommes de terre hongroises ou des lasagnes au thon.")
-print(resp)
+def annouceFood(food):
+    sc.api_call("chat.postMessage", as_user="true", channel=chans['ahtable'], text="J'espère que vous avez faim ce soir pour les aiguillettes de poulet avec des pommes de terre hongroises ou des lasagnes au thon !")
+
+
+    
