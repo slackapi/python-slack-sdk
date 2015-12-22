52090d7be82b48a462bf507cfa4212a67cb5bd92
Peter <petr.demin@nih.gov>
2015-12-22 15:36:55 -0500

    Moved from urllib to requests

--- slackclient/_slackrequest.py
+++ slackclient/_slackrequest.py
@@ -1,16 +1,9 @@
-import time
-from future.moves.urllib.parse import urlparse, urlencode
-from future.moves.urllib.request import urlopen, Request
-from future.moves.urllib.error import HTTPError
+import requests
 
 
 class SlackRequest(object):
-    def __init__(self):
-        pass
-
     def do(self, token, request="?", post_data={}, domain="slack.com"):
-        post_data["token"] = token
-        post_data = urlencode(post_data)
-        url = 'https://{}/api/{}'.format(domain, request)
-        return urlopen(url, post_data.encode('utf-8'))
-
+        return requests.post(
+            'https://{0}/api/{1}'.format(domain, request),
+            data=dict(post_data, token=token),
+        )
