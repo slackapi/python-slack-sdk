1b05d3d0a1b0663abca90234f0fd640bf68168b9
Rick Hanlon II <rickhanlonii@gmail.com>
2016-02-21 13:47:37 +0100

    Make default arguments immutable

--- slackclient/_slackrequest.py
+++ slackclient/_slackrequest.py
@@ -8,7 +8,9 @@ class SlackRequest(object):
     def __init__(self):
         pass
 
-    def do(self, token, request="?", post_data={}, domain="slack.com"):
+    def do(self, token, request="?", post_data=None, domain="slack.com"):
+        if post_data is None:
+            post_data = {}
         post_data["token"] = token
         post_data = urlencode(post_data)
         url = 'https://{}/api/{}'.format(domain, request)
