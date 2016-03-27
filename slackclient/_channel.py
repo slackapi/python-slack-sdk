11d91e047a81efc21ec5985829484496cd34533b
cclauss <cclauss@bluewin.ch>
2016-03-27 20:35:50 +0200

    Update _channel.py

--- slackclient/_channel.py
+++ slackclient/_channel.py
@@ -3,19 +3,14 @@ class Channel(object):
         self.server = server
         self.name = name
         self.id = channel_id
-        self.members = [] if members is None else members
+        self.members = members or []
 
     def __eq__(self, compare_str):
-        if self.name == compare_str or self.name == "#" + compare_str or self.id == compare_str:
-            return True
-        else:
-            return False
+        return compare_str in (self.id, self.name) or "#" + compare_str == self.name
 
     def __str__(self):
-        data = ""
-        for key in list(self.__dict__.keys()):
-            data += "{0} : {1}\n".format(key, str(self.__dict__[key])[:40])
-        return data
+        fmt = "{} : {:.40}"
+        return "\n".join(fmt.format(key, str(value)) for key, value in self.__dict__.items())
 
     def __repr__(self):
         return self.__str__()
