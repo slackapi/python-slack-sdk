38cb3c7e62c5042e411e89e9c877f6600c8e0a17
Christo Goosen <christo.goosen@takectrl.co.za>
2016-03-27 19:11:00 +0200

    Update _slackrequest.py

--- slackclient/_slackrequest.py
+++ slackclient/_slackrequest.py
@@ -5,9 +5,7 @@ class SlackRequest(object):
 
     @staticmethod
     def do(token, request="?", post_data=None, domain="slack.com"):
-        if post_data is None:
-            post_data = {}
-
+        post_data = post_data or {}
         return requests.post(
             'https://{0}/api/{1}'.format(domain, request),
             data=dict(post_data, token=token),
