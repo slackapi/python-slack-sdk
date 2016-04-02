708da0302c6976759d8ba9fab086c138d6e5bd59
lcd1232 <malexey1984@gmail.com>
2016-04-02 21:24:49 +0300

    Added feature to send images via multi-part form

--- slackclient/_slackrequest.py
+++ slackclient/_slackrequest.py
@@ -9,7 +9,7 @@ class SlackRequest(object):
             post_data = {}
 
         if "files" in post_data:
-            temp = dict(element:post_data[element] for element in post_data if not "files" in element)
+            temp = dict([(element, post_data[element]) for element in post_data if not "files" in element])
             return requests.post(
             'https://{0}/api/{1}'.format(domain, request),
             data=dict(temp, token=token), files=post_data["files"]
