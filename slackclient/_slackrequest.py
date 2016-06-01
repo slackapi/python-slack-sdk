5f6e525f11b12de98dfadfc721176caa193541e3
Yoan Blanc <yoan@dosimple.ch>
2016-06-01 19:18:04 +0200

    Add support for nested structure, e.g. attachments.
    > The optional attachments argument should contain a JSON-encoded array of
    > attachments.
    >
    > https://api.slack.com/methods/chat.postMessage
    
    This enables simpler calls like this:
    
        sc.api_call('chat.postMessage', {'attachments': [{'title': 'hello'}]})

--- slackclient/_slackrequest.py
+++ slackclient/_slackrequest.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 
@@ -6,7 +7,12 @@ class SlackRequest(object):
     @staticmethod
     def do(token, request="?", post_data=None, domain="slack.com"):
         post_data = post_data or {}
-        return requests.post(
-            'https://{0}/api/{1}'.format(domain, request),
-            data=dict(post_data, token=token),
-        )
+
+        for k, v in post_data.items():
+            if not isinstance(v, (str, unicode)):
+                post_data[k] = json.dumps(v)
+
+        url = 'https://{0}/api/{1}'.format(domain, request)
+        post_data['token'] = token
+
+        return requests.post(url, data=post_data)
