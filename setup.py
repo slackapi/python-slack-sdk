e0f8dc9d9743524ce928529752b446edbe81f56c
roach <urbandev@gmail.com>
2017-01-06 13:42:07 -0800

    Send user-agent header for Web API calls Programmatically fetch package version for uq header

--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+from setuptools import setup, find_packages
+import codecs
+import os
+import re
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Read the version number from a source file.
+# Why read it, and not import?
+# see https://groups.google.com/d/topic/pypa-dev/0PkjVpcxTzQ/discussion
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
 
 setup(name='slackclient',
-      version='1.0.4',
+      version=find_version('slackclient', 'version.py'),
       description='Python client for Slack.com',
       url='http://github.com/slackapi/python-slackclient',
       author='Ryan Huber',
--- slackclient/_slackrequest.py
+++ slackclient/_slackrequest.py
@@ -3,6 +3,9 @@ import json
 import requests
 import six
 
+import sys
+import platform
+from .version import __version__
 
 class SlackRequest(object):
 
@@ -19,11 +22,22 @@ class SlackRequest(object):
             domain (str): if for some reason you want to send your request to something other
                 than slack.com
         '''
-        post_data = post_data or {}
+
+        user_agent = [] # Construct the user-agent header with the package info, Python version and OS version.
+
+        client_name = __name__.split('.')[0] # __name__ returns 'slackclient._slackrequest', we only want 'slackclient'
+        client_version = __version__ # Version is returned from version.py
+
+        user_agent.append("%s/%s" % (client_name, client_version))
+        user_agent.append("python/%s.%s.%s" % (sys.version_info[0], sys.version_info[1], sys.version_info[2]))
+        user_agent.append("%s/%s" % (platform.system(), platform.release()))
+
+        headers = {'user-agent': ' '.join(map(str, user_agent))}
 
         # Pull file out so it isn't JSON encoded like normal fields.
         # Only do this for requests that are UPLOADING files; downloading files
         # use the 'file' argument to point to a File ID.
+        post_data = post_data or {}
         upload_requests = ['files.upload']
         files = None
         if request in upload_requests:
@@ -36,4 +50,4 @@ class SlackRequest(object):
         url = 'https://{0}/api/{1}'.format(domain, request)
         post_data['token'] = token
 
-        return requests.post(url, data=post_data, files=files)
+        return requests.post(url, headers=headers, data=post_data, files=files)
