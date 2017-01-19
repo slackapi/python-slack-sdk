6ab243fc8592d2d28f39cc698945dfe5bb036cf2
Roach <urbandev@gmail.com>
2017-01-19 15:16:45 -0800

    Add user-agent headers (#160)
    * Send user-agent header for Web API calls

--- tests/test_slackrequest.py
+++ tests/test_slackrequest.py
@@ -1,17 +1,45 @@
 from slackclient._slackrequest import SlackRequest
+from slackclient.version import __version__
 import json
 import os
 
+def test_http_headers(mocker):
+    requests = mocker.patch('slackclient._slackrequest.requests')
+    request = SlackRequest()
+
+    request.do('xoxb-123', 'chat.postMessage', {'text': 'test', 'channel': '#general'})
+    args, kwargs = requests.post.call_args
+
+    assert None != kwargs['headers']['user-agent']
+
+def test_custom_user_agent(mocker):
+    requests = mocker.patch('slackclient._slackrequest.requests')
+    request = SlackRequest()
+
+    request.append_user_agent("fooagent1","0.1")
+    request.append_user_agent("baragent/2","0.2")
+
+    request.do('xoxb-123', 'chat.postMessage', {'text': 'test', 'channel': '#general'})
+    args, kwargs = requests.post.call_args
+
+    # Verify user-agent includes both default and custom agent info
+    assert "slackclient/{}".format(__version__) in kwargs['headers']['user-agent']
+    assert "fooagent1/0.1" in kwargs['headers']['user-agent']
+
+    # verify escaping of slashes in custom agent name
+    assert "baragent:2/0.2" in kwargs['headers']['user-agent']
+
 def test_post_file(mocker):
     requests = mocker.patch('slackclient._slackrequest.requests')
+    request = SlackRequest()
 
-    response = SlackRequest.do('xoxb-123',
+    request.do('xoxb-123',
         'files.upload',
         {'file': open(os.path.join('.', 'tests', 'data', 'slack_logo.png'), 'rb'),
             'filename': 'slack_logo.png'})
+    args, kwargs = requests.post.call_args
 
     assert requests.post.call_count == 1
-    args, kwargs = requests.post.call_args
     assert 'https://slack.com/api/files.upload' == args[0]
     assert {'filename': 'slack_logo.png',
             'token': 'xoxb-123'} == kwargs['data']
@@ -19,11 +47,12 @@ def test_post_file(mocker):
 
 def test_get_file(mocker):
     requests = mocker.patch('slackclient._slackrequest.requests')
+    request = SlackRequest()
 
-    SlackRequest.do('xoxb-123', 'files.info', {'file': 'myFavoriteFileID'})
+    request.do('xoxb-123', 'files.info', {'file': 'myFavoriteFileID'})
+    args, kwargs = requests.post.call_args
 
     assert requests.post.call_count == 1
-    args, kwargs = requests.post.call_args
     assert 'https://slack.com/api/files.info' == args[0]
     assert {'file': "myFavoriteFileID",
             'token': 'xoxb-123'} == kwargs['data']
@@ -31,13 +60,14 @@ def test_get_file(mocker):
 
 def test_post_attachements(mocker):
     requests = mocker.patch('slackclient._slackrequest.requests')
+    request = SlackRequest()
 
-    SlackRequest.do('xoxb-123',
+    request.do('xoxb-123',
                     'chat.postMessage',
                     {'attachments': [{'title': 'hello'}]})
+    args, kwargs = requests.post.call_args
 
     assert requests.post.call_count == 1
-    args, kwargs = requests.post.call_args
     assert 'https://slack.com/api/chat.postMessage' == args[0]
     assert {'attachments': json.dumps([{'title': 'hello'}]),
             'token': 'xoxb-123'} == kwargs['data']
