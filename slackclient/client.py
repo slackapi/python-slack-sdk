dc1c0837fad6239230081f19ded07e926b8efb60
roach <urbandev@gmail.com>
2017-08-30 14:58:35 -0700

    New and improved client.send_rtm_message

--- slackclient/client.py
+++ slackclient/client.py
@@ -136,6 +136,42 @@ class SlackClient(object):
         else:
             raise SlackNotConnected
 
+    def rtm_send_message(self, channel, message, thread=None, reply_broadcast=None):
+        '''
+        Sends a message to a given channel.
+
+        :Args:
+            channel (str) - the string identifier for a channel or channel name (e.g. 'C1234ABC',
+            'bot-test' or '#bot-test')
+            message (message) - the string you'd like to send to the channel
+            thread (str or None) - the parent message ID, if sending to a
+                thread
+            reply_broadcast (bool) - if messaging a thread, whether to
+                also send the message back to the channel
+
+        :Returns:
+            None
+
+        '''
+        found_channel = self.server.channels.find(channel)
+        if found_channel:
+            # If the channel is found in cache, send to it's `send_message` method
+            return found_channel.send_message(
+                message,
+                thread,
+                reply_broadcast,
+            )
+        else:
+            # If the channel is not found in the cache, call the server's `rtm_send_message`
+            # method, passing the message identifier. This identifier must be the channel ID,
+            # rather than the channel name.
+            return self.server.rtm_send_message(
+                channel,
+                message,
+                thread,
+                reply_broadcast
+            )
+
     def process_changes(self, data):
         '''
         Internal method which processes RTM events and modifies the local data store
