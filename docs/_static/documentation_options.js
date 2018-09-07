9f3708f1921ef9f8affffa63b34144881a108383
roach <urbandev@gmail.com>
2018-09-07 11:25:48 -0700

    Regenerated docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,5 +1,5 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '1.0.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
