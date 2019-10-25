slackapi/python-slack-sdk

commit 854920dfb2a25fd9b5e58953c6d25089ca69451d

Some responses don't return json. Correcting initial assumption.

slack/web/base_client.py
@@ -245,24 +245,29 @@ class BaseClient:
         Returns:
             A dictionary of the response data.
         """
+        session = None
         if self.session and not self.session.closed:
-            async with self.session.request(http_verb, api_url, **req_args) as res:
-                return {
-                    "data": await res.json(),
-                    "headers": res.headers,
-                    "status_code": res.status,
-                }
-        async with aiohttp.ClientSession(
-            loop=self._event_loop,
-            timeout=aiohttp.ClientTimeout(total=self.timeout),
-            auth=req_args.pop("auth"),
-        ) as session:
-            async with session.request(http_verb, api_url, **req_args) as res:
-                return {
-                    "data": await res.json(),
-                    "headers": res.headers,
-                    "status_code": res.status,
-                }
+            session = self.session
+        else:
+            session = aiohttp.ClientSession(
+                loop=self._event_loop,
+                timeout=aiohttp.ClientTimeout(total=self.timeout),
+                auth=req_args.pop("auth", None),
+            )
+
+        response = None
+        async with session.request(http_verb, api_url, **req_args) as res:
+            data = {}
+            try:
+                data = await res.json()
+            except aiohttp.ContentTypeError:
+                self._logger.debug(
+                    f"No response data returned from the following API call: {api_url}."
+                )
+            response = {"data": data, "headers": res.headers, "status_code": res.status}
+
+        await session.close()
+        return response
 
     @staticmethod
     def _get_user_agent():

commit 4f167d0eedab1159d2e3ba775fecdc153225e6f5

Make sure we do not close the running session
We must only close the temporary session used for the request.

Fixes: 854920dfb2a2

Signed-off-by: Fatih Acar <f.acar@criteo.com>

slack/web/base_client.py
@@ -246,7 +246,8 @@ class BaseClient:
             A dictionary of the response data.
         """
         session = None
-        if self.session and not self.session.closed:
+        use_running_session = self.session and not self.session.closed
+        if use_running_session:
             session = self.session
         else:
             session = aiohttp.ClientSession(
@@ -265,7 +266,8 @@ class BaseClient:
                 )
             response = {"data": data, "headers": res.headers, "status_code": res.status}
 
-        await session.close()
+        if not use_running_session:
+            await session.close()
         return response
 
     @staticmethod