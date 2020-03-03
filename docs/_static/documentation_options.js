2fb4dac5a6de45c0261a020719170e7d7d5a4ea0
Kazuhiro Sera <seratch@gmail.com>
2020-03-03 18:03:54 +0900

    Run ./docs.sh

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -3,6 +3,7 @@ var DOCUMENTATION_OPTIONS = {
     VERSION: '1.0.1',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
