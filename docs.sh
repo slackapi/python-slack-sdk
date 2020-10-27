afac3dbbcba0e381f044044e7bba3c6d1632e326
Kazuhiro Sera <seratch@gmail.com>
2020-10-27 14:42:26 +0900

    Remove _static directory to avoid potential conflicts with GH pages

--- docs.sh
+++ docs.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env bash
 
-sphinx-build -E -c ./docs-src/_themes/slack/ -b html docs-src docs && touch ./docs/.nojekyll
+sphinx-build -E -c ./docs-src/_themes/slack/ -b html docs-src docs \
+  && touch ./docs/.nojekyll \
+  && cd docs/ \
+  && mv _static assets \
+  && find . -name '*.html' | xargs sed -i '' 's/_static/assets/'g \
+  && cd -
