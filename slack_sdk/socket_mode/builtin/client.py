b1b43dd43d1b703db772e1a6aba74465a9781fb4
Kazuhiro Sera <seratch@gmail.com>
2020-12-03 20:13:39 +0900

    Update built-in client

--- slack_sdk/socket_mode/builtin/client.py
+++ slack_sdk/socket_mode/builtin/client.py
@@ -4,11 +4,9 @@ from concurrent.futures.thread import ThreadPoolExecutor
 from logging import Logger
 from queue import Queue
 from threading import Lock
-from typing import Union, Optional, List, Callable, Tuple
+from typing import Union, Optional, List, Callable
 
-import websocket
-from websocket import WebSocketApp
-
+from .connection import Connection
 from slack_sdk.socket_mode.client import BaseSocketModeClient
 from slack_sdk.socket_mode.listeners import (
     WebSocketMessageListener,
@@ -40,20 +38,21 @@ class SocketModeClient(BaseSocketModeClient):
     current_app_executor: ThreadPoolExecutor
     current_app_monitor: ThreadPoolExecutor
     current_app_monitor_started: bool
+
     message_processor: ThreadPoolExecutor
     message_workers: ThreadPoolExecutor
 
-    current_session: Optional[WebSocketApp]
+    current_session: Optional[Connection]
 
     auto_reconnect_enabled: bool
     default_auto_reconnect_enabled: bool
+    trace_enabled: bool
 
     connect_operation_lock: Lock
 
-    on_open_listeners: List[Callable[[WebSocketApp], None]]
-    on_message_listeners: List[Callable[[WebSocketApp, str], None]]
-    on_error_listeners: List[Callable[[WebSocketApp, Exception], None]]
-    on_close_listeners: List[Callable[[WebSocketApp], None]]
+    on_message_listeners: List[Callable[[str], None]]
+    on_error_listeners: List[Callable[[Exception], None]]
+    on_close_listeners: List[Callable[[int, Optional[str]], None]]
 
     def __init__(
         self,
@@ -61,27 +60,20 @@ class SocketModeClient(BaseSocketModeClient):
         logger: Optional[Logger] = None,
         web_client: Optional[WebClient] = None,
         auto_reconnect_enabled: bool = True,
+        trace_enabled: bool = False,
         ping_interval: float = 10,
         concurrency: int = 10,
-        trace_enabled: bool = False,
-        http_proxy_host: Optional[str] = None,
-        http_proxy_port: Optional[int] = None,
-        http_proxy_auth: Optional[Tuple[str, str]] = None,
-        proxy_type: Optional[str] = None,
-        on_open_listeners: Optional[List[Callable[[WebSocketApp], None]]] = None,
-        on_message_listeners: Optional[
-            List[Callable[[WebSocketApp, str], None]]
-        ] = None,
-        on_error_listeners: Optional[
-            List[Callable[[WebSocketApp, Exception], None]]
-        ] = None,
-        on_close_listeners: Optional[List[Callable[[WebSocketApp], None]]] = None,
+        proxy: Optional[str] = None,
+        on_message_listeners: Optional[List[Callable[[str], None]]] = None,
+        on_error_listeners: Optional[List[Callable[[Exception], None]]] = None,
+        on_close_listeners: Optional[List[Callable[[int, Optional[str]], None]]] = None,
     ):
         self.app_token = app_token
         self.logger = logger or logging.getLogger(__name__)
         self.web_client = web_client or WebClient()
         self.default_auto_reconnect_enabled = auto_reconnect_enabled
         self.auto_reconnect_enabled = self.default_auto_reconnect_enabled
+        self.trace_enabled = trace_enabled
         self.ping_interval = ping_interval
         self.wss_uri = self.issue_new_wss_url()
         self.message_queue = Queue()
@@ -99,70 +91,66 @@ class SocketModeClient(BaseSocketModeClient):
         self.message_processor.submit(self.process_messages)
         self.message_workers = ThreadPoolExecutor(max_workers=concurrency)
 
-        # NOTE: only global settings is provided by the library
-        websocket.enableTrace(trace_enabled)
+        self.proxy = proxy
 
-        self.http_proxy_host = http_proxy_host
-        self.http_proxy_port = http_proxy_port
-        self.http_proxy_auth = http_proxy_auth
-        self.proxy_type = proxy_type
-
-        self.on_open_listeners = on_open_listeners or []
         self.on_message_listeners = on_message_listeners or []
         self.on_error_listeners = on_error_listeners or []
         self.on_close_listeners = on_close_listeners or []
 
+    def session_id(self) -> Optional[str]:
+        if self.current_session is not None:
+            return self.current_session.session_id
+        return None
+
     def is_connected(self) -> bool:
-        return self.current_session is not None
+        return self.current_session is not None \
+               and self.current_session.is_active()
 
     def connect(self) -> None:
-        def on_open(ws: WebSocketApp):
-            if self.logger.level <= logging.DEBUG:
-                self.logger.debug("on_open invoked")
-            for listener in self.on_open_listeners:
-                listener(ws)
-
-        def on_message(ws: WebSocketApp, message: str):
+        def on_message(message: str):
             if self.logger.level <= logging.DEBUG:
                 self.logger.debug(f"on_message invoked: (message: {message})")
             self.enqueue_message(message)
             for listener in self.on_message_listeners:
-                listener(ws, message)
+                listener(message)
 
-        def on_error(ws: WebSocketApp, error: Exception):
+        def on_error(error: Exception):
             self.logger.error(
-                f"on_error invoked (error: {type(error).__name__}, message: {error})"
+                f"on_error invoked (session id: {self.session_id()}, "
+                f"error: {type(error).__name__}, message: {error})",
+                error,
             )
             for listener in self.on_error_listeners:
-                listener(ws, error)
+                listener(error)
 
-        def on_close(ws: WebSocketApp):
+        def on_close(code: int, reason: Optional[str] = None):
             if self.logger.level <= logging.DEBUG:
-                self.logger.debug("on_close invoked")
+                self.logger.debug(f"on_close invoked (session id: {self.session_id()})")
             if self.auto_reconnect_enabled:
-                self.logger.info("Received CLOSE event. Going to reconnect...")
+                self.logger.info(
+                    "Received CLOSE event. Going to reconnect... "
+                    f"(session id: {self.session_id()})"
+                )
                 self.connect_to_new_endpoint()
             for listener in self.on_close_listeners:
-                listener(ws)
+                listener(code, reason)
 
-        old_session: Optional[WebSocketApp] = self.current_session
-        self.current_session = websocket.WebSocketApp(
-            self.wss_uri,
-            on_open=on_open,
-            on_message=on_message,
-            on_error=on_error,
-            on_close=on_close,
+        old_session: Optional[Connection] = self.current_session
+        self.current_session = Connection(
+            url=self.wss_uri,
+            logger=self.logger,
+            ping_interval=self.ping_interval,
+            trace_enabled=self.trace_enabled,
+            proxy=self.proxy,
+            on_message_listener=on_message,
+            on_error_listener=on_error,
+            on_close_listener=on_close,
         )
+        self.current_session.connect()
         self.auto_reconnect_enabled = self.default_auto_reconnect_enabled
 
         def run_current_session():
-            self.current_session.run_forever(
-                ping_interval=self.ping_interval,
-                http_proxy_host=self.http_proxy_host,
-                http_proxy_port=self.http_proxy_port,
-                http_proxy_auth=self.http_proxy_auth,
-                proxy_type=self.proxy_type,
-            )
+            self.current_session.run_forever()
 
         def monitor_current_session():
             while True:
@@ -170,16 +158,17 @@ class SocketModeClient(BaseSocketModeClient):
                 try:
                     if self.auto_reconnect_enabled and (
                         self.current_session is None
-                        or self.current_session.sock is None
+                        or not self.current_session.is_active()
                     ):
                         self.logger.info(
-                            "The session seems to be already closed. Going to reconnect..."
+                            "The session seems to be already closed. Going to reconnect... "
+                            f"(session id: {self.session_id()})"
                         )
                         self.connect_to_new_endpoint()
                 except Exception as e:
                     self.logger.error(
                         "Failed to check the current session or reconnect to the server "
-                        f"(error: {type(e).__name__}, message: {e})"
+                        f"(session id: {self.session_id()}, error: {type(e).__name__}, message: {e})"
                     )
 
         self.current_app_executor.submit(run_current_session)
@@ -187,7 +176,9 @@ class SocketModeClient(BaseSocketModeClient):
             self.current_app_monitor.submit(monitor_current_session)
             self.current_app_monitor_started = True
 
-        self.logger.info("A new session has been established")
+        self.logger.info(
+            f"A new session has been established (session id: {self.session_id()})"
+        )
         if old_session is not None:
             old_session.close()
 
@@ -197,7 +188,9 @@ class SocketModeClient(BaseSocketModeClient):
 
     def send_message(self, message: str) -> None:
         if self.logger.level <= logging.DEBUG:
-            self.logger.debug(f"Sending a message: {message}")
+            self.logger.debug(
+                f"Sending a message (session id: {self.session_id()}, message: {message})"
+            )
         self.current_session.send(message)
 
     def close(self):
