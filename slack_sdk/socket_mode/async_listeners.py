7f1a1171280e005323b8145ba09ce60833e6e04d
Kazuhiro Sera <seratch@gmail.com>
2021-01-10 10:50:44 +0900

    Add noqa comments

--- slack_sdk/socket_mode/async_listeners.py
+++ slack_sdk/socket_mode/async_listeners.py
@@ -5,15 +5,15 @@ from slack_sdk.socket_mode.request import SocketModeRequest
 
 class AsyncWebSocketMessageListener(Callable):
     async def __call__(
-        client: "AsyncBaseSocketModeClient",
+        client: "AsyncBaseSocketModeClient",  # noqa: F821
         message: dict,
         raw_message: Optional[str] = None,
-    ):
+    ):  # noqa: F821
         raise NotImplementedError()
 
 
 class AsyncSocketModeRequestListener(Callable):
     async def __call__(
-        client: "AsyncBaseSocketModeClient", request: SocketModeRequest,
-    ):
+        client: "AsyncBaseSocketModeClient", request: SocketModeRequest,  # noqa: F821
+    ):  # noqa: F821
         raise NotImplementedError()
--- slack_sdk/socket_mode/listeners.py
+++ slack_sdk/socket_mode/listeners.py
@@ -5,15 +5,15 @@ from slack_sdk.socket_mode.request import SocketModeRequest
 
 class WebSocketMessageListener:
     def __call__(
-        client: "BaseSocketModeClient",
+        client: "BaseSocketModeClient",  # noqa: F821
         message: dict,
         raw_message: Optional[str] = None,
-    ):
+    ):  # noqa: F821
         raise NotImplementedError()
 
 
 class SocketModeRequestListener:
     def __call__(
-        client: "BaseSocketModeClient", request: SocketModeRequest
+        client: "BaseSocketModeClient", request: SocketModeRequest  # noqa: F821
     ):  # noqa: F821
         raise NotImplementedError()
