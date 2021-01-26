00ff4e219be68cf324e21fc5ddea8f4b5571fc0e
Kazuhiro Sera <seratch@gmail.com>
2021-01-26 12:26:49 +0900

    Fix #926 Handle closing aiohttp.ClientSession in socket mode

---
+++ integration_tests/samples/issues/issue_926.py
@@ -0,0 +1,30 @@
+# ------------------
+# Only for running this script here
+import sys
+from os.path import dirname
+
+sys.path.insert(1, f"{dirname(__file__)}/../../..")
+# ------------------
+
+import asyncio
+import logging
+import os
+
+from slack_sdk.socket_mode.aiohttp import SocketModeClient
+
+logging.basicConfig(level=logging.DEBUG)
+
+async def main():
+    client = SocketModeClient(app_token=os.environ["SLACK_APP_TOKEN"])
+    await client.connect()
+    await asyncio.sleep(3)
+    await client.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+
+# The issue:
+# ERROR:asyncio:Unclosed client session
+# client_session: <aiohttp.client.ClientSession object at 0x10e1085e0>
+# INFO:slack_sdk.socket_mode.aiohttp:The session has been abandoned
