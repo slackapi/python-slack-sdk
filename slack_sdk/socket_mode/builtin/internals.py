5df42367d64d655f28c0b64599b7012b4863dcfa
Kazuhiro Sera <seratch@gmail.com>
2021-01-29 00:13:43 +0900

    Remove noisy logging for empty messages in all trace mode

--- slack_sdk/socket_mode/builtin/internals.py
+++ slack_sdk/socket_mode/builtin/internals.py
@@ -124,7 +124,8 @@ def _receive_messages(
         )
         received_bytes = sock.recv(size)
         if all_message_trace_enabled:
-            logger.debug(f"Received bytes: {received_bytes}")
+            if len(received_bytes):
+                logger.debug(f"Received bytes: {received_bytes}")
         return received_bytes
 
     return _fetch_messages(
