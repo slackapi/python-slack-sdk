3bbca46ce28393815c51bd5d9a612120c8021565
Kazuhiro Sera <seratch@gmail.com>
2021-02-01 20:52:23 +0900

    Fix #437 Add SCIM API Support

---
+++ integration_tests/scim/test_scim_client_read.py
@@ -0,0 +1,81 @@
+import logging
+import os
+import time
+import unittest
+
+from integration_tests.env_variable_names import (
+    SLACK_SDK_TEST_GRID_ORG_ADMIN_USER_TOKEN,
+)
+from slack_sdk.scim import SCIMClient, SCIMResponse
+from slack_sdk.scim.v1.response import Errors
+from slack_sdk.scim.v1.user import User, UserName, UserEmail
+
+
+class TestSCIMClient(unittest.TestCase):
+    def setUp(self):
+        self.logger = logging.getLogger(__name__)
+        self.bot_token = os.environ[SLACK_SDK_TEST_GRID_ORG_ADMIN_USER_TOKEN]
+        self.client: SCIMClient = SCIMClient(token=self.bot_token)
+
+    def tearDown(self):
+        pass
+
+    def test_api_call(self):
+        response: SCIMResponse = self.client.api_call(
+            http_verb="GET", path="Users", query_params={"startIndex": 1, "count": 1}
+        )
+        self.assertIsNotNone(response)
+
+        self.logger.info(response.snake_cased_body)
+        self.assertEqual(response.snake_cased_body["start_index"], 1)
+        self.assertIsNotNone(response.snake_cased_body["resources"][0]["id"])
+
+    def test_lookup_users(self):
+        search_result = self.client.search_users(start_index=1, count=1)
+        self.assertIsNotNone(search_result)
+
+        self.logger.info(search_result.snake_cased_body)
+        self.assertEqual(search_result.snake_cased_body["start_index"], 1)
+        self.assertIsNotNone(search_result.snake_cased_body["resources"][0]["id"])
+        self.assertEqual(
+            search_result.users[0].id,
+            search_result.snake_cased_body["resources"][0]["id"],
+        )
+
+        read_result = self.client.read_user(search_result.users[0].id)
+        self.assertIsNotNone(read_result)
+        self.logger.info(read_result.snake_cased_body)
+        self.assertEqual(read_result.user.id, search_result.users[0].id)
+
+    def test_lookup_users_error(self):
+        # error
+        error_result = self.client.search_users(start_index=1, count=1, filter="foo")
+        self.assertEqual(error_result.errors.code, 400)
+        self.assertEqual(
+            error_result.errors.description, "no_filters (is_aggregate_call=1)"
+        )
+
+    def test_lookup_groups(self):
+        search_result = self.client.search_groups(start_index=1, count=1)
+        self.assertIsNotNone(search_result)
+
+        self.logger.info(search_result.snake_cased_body)
+        self.assertEqual(search_result.snake_cased_body["start_index"], 1)
+        self.assertIsNotNone(search_result.snake_cased_body["resources"][0]["id"])
+        self.assertEqual(
+            search_result.groups[0].id,
+            search_result.snake_cased_body["resources"][0]["id"],
+        )
+
+        read_result = self.client.read_group(search_result.groups[0].id)
+        self.assertIsNotNone(read_result)
+        self.logger.info(read_result.snake_cased_body)
+        self.assertEqual(read_result.group.id, search_result.groups[0].id)
+
+    def test_lookup_groups_error(self):
+        # error
+        error_result = self.client.search_groups(start_index=1, count=1, filter="foo")
+        self.assertEqual(error_result.errors.code, 400)
+        self.assertEqual(
+            error_result.errors.description, "no_filters (is_aggregate_call=1)"
+        )
---
+++ integration_tests/scim/test_scim_client_write.py
@@ -0,0 +1,94 @@
+import logging
+import os
+import time
+import unittest
+
+from integration_tests.env_variable_names import (
+    SLACK_SDK_TEST_GRID_ORG_ADMIN_USER_TOKEN,
+)
+from slack_sdk.scim import SCIMClient
+from slack_sdk.scim.v1.group import Group, GroupMember
+from slack_sdk.scim.v1.user import User, UserName, UserEmail
+
+
+class TestSCIMClient(unittest.TestCase):
+    def setUp(self):
+        self.logger = logging.getLogger(__name__)
+        self.bot_token = os.environ[SLACK_SDK_TEST_GRID_ORG_ADMIN_USER_TOKEN]
+        self.client: SCIMClient = SCIMClient(token=self.bot_token)
+
+    def tearDown(self):
+        pass
+
+    def test_user_crud(self):
+        now = str(time.time())[:10]
+        user = User(
+            user_name=f"user_{now}",
+            name=UserName(given_name="Kaz", family_name="Sera"),
+            emails=[UserEmail(value=f"seratch+{now}@example.com")],
+            schemas=[
+                "urn:scim:schemas:core:1.0",
+                # "urn:scim:schemas:extension:enterprise:1.0",
+                # "urn:scim:schemas:extension:slack:guest:1.0"
+            ],
+            # additional_fields={
+            #     "urn:scim:schemas:extension:slack:guest:1.0": {
+            #         "type": "multi",
+            #         "expiration": "2022-11-30T23:59:59Z"
+            #     }
+            # }
+        )
+        creation = self.client.create_user(user)
+        self.assertEqual(creation.status_code, 201)
+
+        patch_result = self.client.patch_user(
+            id=creation.user.id,
+            partial_user=User(
+                user_name=f"user_{now}_2",
+                name=UserName(given_name="Kazuhiro", family_name="Sera"),
+            ),
+        )
+        self.assertEqual(patch_result.status_code, 200)
+
+        updated_user = creation.user
+        updated_user.name = UserName(given_name="Foo", family_name="Bar")
+        update_result = self.client.update_user(user=updated_user)
+        self.assertEqual(update_result.status_code, 200)
+
+        delete_result = self.client.delete_user(updated_user.id)
+        self.assertEqual(delete_result.status_code, 200)
+
+    def test_group_crud(self):
+        now = str(time.time())[:10]
+
+        user = User(
+            user_name=f"user_{now}",
+            name=UserName(given_name="Kaz", family_name="Sera"),
+            emails=[UserEmail(value=f"seratch+{now}@example.com")],
+            schemas=["urn:scim:schemas:core:1.0"],
+        )
+        user_creation = self.client.create_user(user)
+        group = Group(
+            display_name=f"TestGroup_{now}",
+            members=[GroupMember(value=user_creation.user.id)],
+        )
+        creation = self.client.create_group(group)
+        self.assertEqual(creation.status_code, 201)
+
+        group = creation.group
+
+        patch_result = self.client.patch_group(
+            id=group.id,
+            partial_group=Group(
+                display_name=f"Test Group{now}_2",
+            ),
+        )
+        self.assertEqual(patch_result.status_code, 204)
+
+        updated_group = group
+        updated_group.display_name = f"Test Group{now}_3"
+        update_result = self.client.update_group(updated_group)
+        self.assertEqual(update_result.status_code, 200)
+
+        delete_result = self.client.delete_group(updated_group.id)
+        self.assertEqual(delete_result.status_code, 204)
