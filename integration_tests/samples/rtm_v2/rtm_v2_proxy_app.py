10c7d2f33d34e5a158795701b2d9bc71bbb1c120
Kazuhiro Sera <seratch@gmail.com>
2021-02-11 08:52:55 +0900

    Add RTM v2 client example too

---
+++ integration_tests/samples/rtm_v2/rtm_v2_proxy_app.py
@@ -0,0 +1,46 @@
+# ------------------
+# Only for running this script here
+import sys
+from os.path import dirname
+
+sys.path.insert(1, f"{dirname(__file__)}/../../..")
+# ------------------
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s.%(msecs)03d %(levelname)s %(filename)s (%(lineno)s): %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+)
+logger = logging.getLogger(__name__)
+
+import os
+from slack_sdk.rtm.v2 import RTMClient
+from integration_tests.env_variable_names import SLACK_SDK_TEST_CLASSIC_APP_BOT_TOKEN
+
+# pip3 install proxy.py
+# proxy --port 9000 --log-level d
+proxy_url = "http://localhost:9000"
+
+if __name__ == "__main__":
+    rtm = RTMClient(
+        token=os.environ.get(SLACK_SDK_TEST_CLASSIC_APP_BOT_TOKEN),
+        trace_enabled=True,
+        all_message_trace_enabled=True,
+        proxy=proxy_url,
+    )
+
+    @rtm.on("message")
+    def handle(client: RTMClient, event: dict):
+        client.web_client.reactions_add(
+            channel=event["channel"],
+            timestamp=event["ts"],
+            name="eyes",
+        )
+
+    @rtm.on("*")
+    def handle(client: RTMClient, event: dict):
+        logger.info(event)
+
+    rtm.start()
