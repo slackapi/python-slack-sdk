8f65b9410e04be351b98f6b551b5edfb456b1de2
Kazuhiro Sera <seratch@gmail.com>
2021-08-02 21:37:32 +0900

    Ugprade black code formatter to the latest version

--- setup.py
+++ setup.py
@@ -25,12 +25,12 @@ validate_dependencies = [
     "pytest-cov>=2,<3",
     "codecov>=2,<3",
     "flake8>=3,<4",
-    "black==21.6b0",
+    "black==21.7b0",
     "psutil>=5,<6",
     "databases>=0.3",
 ]
 codegen_dependencies = [
-    "black==21.6b0",
+    "black==21.7b0",
 ]
 
 needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
