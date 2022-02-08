1bece4f325b6af6433761c3bbfc1b63e243090c2
Kazuhiro Sera <ksera@slack-corp.com>
2022-02-08 18:24:48 +0900

    Fix a bug where some of the files.remote API parameters do not work since v3.10

--- slack_sdk/web/async_client.py
+++ slack_sdk/web/async_client.py
@@ -2794,14 +2794,10 @@ class AsyncWebClient(AsyncBaseClient):
         )
         files = None
         # preview_image (file): Preview of the document via multipart/form-data.
-        if "preview_image" in kwargs or "indexable_file_contents" in kwargs:
+        if preview_image is not None or indexable_file_contents is not None:
             files = {
-                "preview_image": preview_image
-                if preview_image is not None
-                else kwargs.pop("preview_image"),
-                "indexable_file_contents": indexable_file_contents
-                if indexable_file_contents is not None
-                else kwargs.pop("indexable_file_contents"),
+                "preview_image": preview_image,
+                "indexable_file_contents": indexable_file_contents,
             }
 
         return await self.api_call(
@@ -2834,16 +2830,14 @@ class AsyncWebClient(AsyncBaseClient):
                 "file": file,
                 "title": title,
                 "filetype": filetype,
-                "indexable_file_contents": indexable_file_contents,
             }
         )
         files = None
         # preview_image (file): Preview of the document via multipart/form-data.
-        if "preview_image" in kwargs:
+        if preview_image is not None or indexable_file_contents is not None:
             files = {
-                "preview_image": preview_image
-                if preview_image is not None
-                else kwargs.pop("preview_image")
+                "preview_image": preview_image,
+                "indexable_file_contents": indexable_file_contents,
             }
 
         return await self.api_call(
