3f2edd4026e3201879fe42b09ab1e5db6383ac1a
Matt Billenstein <matt@vazor.com>
2022-07-24 20:37:46 +0000

    Raise an exception when reading from socket and getting no data.
    Returning and empty bytes object causes an infinite loop between this and the
    fetch function which in some cases can cause 100% cpu utilization (this happens
    under gevent).
    
    Also the exception handling here can't just return an empty bytes for the same
    reason - the now dead connection cannot be replaced by code that uses this
    code.

--- slack_sdk/socket_mode/builtin/internals.py
+++ slack_sdk/socket_mode/builtin/internals.py
@@ -199,19 +199,12 @@ def _receive_messages(
     def receive(specific_buffer_size: Optional[int] = None):
         size = specific_buffer_size if specific_buffer_size is not None else receive_buffer_size
         with sock_receive_lock:
-            try:
-                received_bytes = sock.recv(size)
-                if all_message_trace_enabled:
-                    if len(received_bytes) > 0:
-                        logger.debug(f"Received bytes: {received_bytes}")
-                return received_bytes
-            except OSError as e:
-                # For Linux/macOS, errno.EBADF is the expected error for bad connections.
-                # The errno.ENOTSOCK can be sent when running on Windows OS.
-                if e.errno in (errno.EBADF, errno.ENOTSOCK):
-                    logger.debug("The connection seems to be already closed.")
-                    return bytes()
-                raise e
+            received_bytes = sock.recv(size)
+            if not received_bytes:
+                raise Exception('socket is dead')
+            if all_message_trace_enabled:
+                logger.debug(f"Received bytes: {received_bytes}")
+            return received_bytes
 
     return _fetch_messages(
         messages=[],
