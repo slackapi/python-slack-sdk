70ce80d4955867ec05d80051dac78b24f15a85e6
Matt Billenstein <matt@vazor.com>
2022-07-25 02:10:54 +0000

    Raise ConnectionError - a subclass of OSError

--- slack_sdk/socket_mode/builtin/internals.py
+++ slack_sdk/socket_mode/builtin/internals.py
@@ -25,7 +25,7 @@ def _parse_connect_response(sock: Socket) -> Tuple[Optional[int], str]:
         while True:
             c = sock.recv(1)
             if not c:
-                raise Exception('server closed connection')
+                raise ConnectionError('Connection is closed')
             line.append(c)
             if c == b"\n":
                 break
@@ -118,7 +118,7 @@ def _read_http_response_line(sock: ssl.SSLSocket) -> str:
     while True:
         b: bytes = sock.recv(1)
         if not b:
-            raise Exception('server closed connection')
+            raise ConnectionError('Connection is closed')
         c: str = b.decode("utf-8")
         if c == "\r":
             break
@@ -206,7 +206,7 @@ def _receive_messages(
         with sock_receive_lock:
             received_bytes = sock.recv(size)
             if not received_bytes:
-                raise Exception('server closed connection')
+                raise ConnectionError('Connection is closed')
             if all_message_trace_enabled:
                 logger.debug(f"Received bytes: {received_bytes}")
             return received_bytes
