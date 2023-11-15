edc04aae6c927e40f882c487f5e5918a3c25ddd5
Kazuhiro Sera <seratch@gmail.com>
2023-11-15 14:33:49 +0900

    Fix #1424 Add file input block element support (#1425)

--- slack_sdk/models/blocks/block_elements.py
+++ slack_sdk/models/blocks/block_elements.py
@@ -1625,6 +1625,55 @@ class NumberInputElement(InputInteractiveElement):
 
 
 # -------------------------------------------------
+# File Input Element
+# -------------------------------------------------
+
+
+class FileInputElement(InputInteractiveElement):
+    type = "file_input"
+
+    @property
+    def attributes(self) -> Set[str]:
+        return super().attributes.union(
+            {
+                "filetypes",
+                "max_files",
+            }
+        )
+
+    def __init__(
+        self,
+        *,
+        action_id: Optional[str] = None,
+        filetypes: Optional[List[str]] = None,
+        max_files: Optional[int] = None,
+        **others: dict,
+    ):
+        """
+        https://api.slack.com/reference/block-kit/block-elements#file_input
+
+        Args:
+            action_id (required): An identifier for the input value when the parent modal is submitted.
+                You can use this when you receive a view_submission payload to identify the value of the input element.
+                Should be unique among all other action_ids in the containing block. Maximum length is 255 characters.
+            filetypes: An array of valid file extensions that will be accepted for this element.
+                All file extensions will be accepted if filetypes is not specified.
+                This validation is provided for convenience only,
+                and you should perform your own file type validation based on what you expect to receive.
+            max_files: Maximum number of files that can be uploaded for this file_input element.
+                Minimum of 1, maximum of 10. Defaults to 10 if not specified.
+        """
+        super().__init__(
+            type=self.type,
+            action_id=action_id,
+        )
+        show_unknown_key_warning(self, others)
+
+        self.filetypes = filetypes
+        self.max_files = max_files
+
+
+# -------------------------------------------------
 # Radio Buttons Select
 # -------------------------------------------------
 
