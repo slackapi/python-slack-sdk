56f71273cac79633e84d94a971fb2a71765f4e23
Aliaksei Urbanski <aliaksei.urbanski@gmail.com>
2023-12-07 01:54:05 +0300

    Update run_*.sh scripts (#1443)
    The Maintainers Guide says that no formatter nor code analyzer
    is involved when you run unit tests, so I suppose black was
    added by mistake and should be removed.
    Also, using set -e allows you to avoid chaining of commands.
    
    These changes:
      * remove black from run_*_tests.sh scripts
      * add set -e to unchain commands in scripts
      * simplify handling of a test target

--- scripts/run_validation.sh
+++ scripts/run_validation.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 # ./scripts/run_validation.sh
 
+set -e
+
 script_dir=`dirname $0`
 cd ${script_dir}/..
-pip install -U pip && \
-  pip install -r requirements/testing.txt && \
-  pip install -r requirements/optional.txt && \
-  black slack_sdk/ slack/ tests/ integration_tests/ && \
-  python setup.py codegen && \
-  python setup.py validate
+pip install -U pip
+pip install -r requirements/testing.txt \
+  -r requirements/optional.txt
+
+black --check tests/ integration_tests/
+# TODO: resolve linting errors for tests
+# flake8 tests/ integration_tests/
+
+python setup.py codegen
+python setup.py validate
