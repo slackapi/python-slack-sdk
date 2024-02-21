fe45e0b9f53815baf41d029c2223a2d3d1bd6c59
Steve Kowalik <steven@wedontsleep.org>
2024-02-22 04:55:27 +1100

    Unpin moto, and support moto 5.0 changes
    To stop testing with old versions of moto, unpin it from version 3, to
    the latest, depending on the Python version being used. Support both
    version 4 and 5 in the test suite.

--- tests/slack_sdk/oauth/state_store/test_amazon_s3.py
+++ tests/slack_sdk/oauth/state_store/test_amazon_s3.py
@@ -1,22 +1,26 @@
 import unittest
 
 import boto3
-from moto import mock_s3
+
+try:
+    from moto import mock_aws
+except ImportError:
+    from moto import mock_s3 as mock_aws
 from slack_sdk.oauth.state_store.amazon_s3 import AmazonS3OAuthStateStore
 
 
 class TestAmazonS3(unittest.TestCase):
-    mock_s3 = mock_s3()
+    mock_aws = mock_aws()
     bucket_name = "test-bucket"
 
     def setUp(self):
-        self.mock_s3.start()
+        self.mock_aws.start()
         s3 = boto3.resource("s3")
         bucket = s3.Bucket(self.bucket_name)
         bucket.create(CreateBucketConfiguration={"LocationConstraint": "af-south-1"})
 
     def tearDown(self):
-        self.mock_s3.stop()
+        self.mock_aws.stop()
 
     def test_instance(self):
         store = AmazonS3OAuthStateStore(
