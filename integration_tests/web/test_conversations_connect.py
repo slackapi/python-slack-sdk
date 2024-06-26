44194d4d6e82f50e7b00be9472ee21080a0d4e9d
William Bergamin <wbergamin@slack-corp.com>
2024-06-26 10:02:33 -0400

    Formate tests

--- integration_tests/web/test_conversations_connect.py
+++ integration_tests/web/test_conversations_connect.py
@@ -81,7 +81,9 @@ class TestWebClient(unittest.TestCase):
                 invite_id=invite["invite_id"],
             )
 
-            sender_approval = sender.conversations_approveSharedInvite(invite_id=invite["invite_id"], team_id=connect_team_id)
+            sender_approval = sender.conversations_approveSharedInvite(
+                invite_id=invite["invite_id"], team_id=connect_team_id
+            )
             self.assertIsNone(sender_approval["error"])
 
             downgrade = sender.conversations_externalInvitePermissions_set(
