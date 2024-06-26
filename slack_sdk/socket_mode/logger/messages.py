47ad0ddcb7d085e3e651a82d28bfe9aefd05693a
William Bergamin <wbergamin@slack-corp.com>
2024-06-26 17:20:39 -0400

    Add example in comment

--- slack_sdk/socket_mode/logger/messages.py
+++ slack_sdk/socket_mode/logger/messages.py
@@ -2,5 +2,5 @@ import re
 
 
 def debug_message_redact(message: str) -> str:
-    xwfp_token_pattern = re.compile(r"\"xwfp-[A-Za-z0-9\-]+\"")
+    xwfp_token_pattern = re.compile(r"\"xwfp-[A-Za-z0-9\-]+\"")  # ex: "xwfp-abc-ABC-1234"
     return re.sub(xwfp_token_pattern, "[[REDACTED]]", message)
