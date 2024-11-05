4fadba088927f57b4d752c2eab2959d2b162e61f
Kazuhiro Sera <seratch@gmail.com>
2024-11-05 09:53:15 +0900

    Remove unused sphinx from the documentation requirements

--- scripts/uninstall_all.sh
+++ scripts/uninstall_all.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 pip uninstall -y slack-sdk && \
-  pip freeze | grep -v "^-e" | xargs pip uninstall -y
+  pip freeze | grep -v "^-e" | sed 's/@.*//' | sed 's/\=\=.*//' | xargs pip uninstall -y
