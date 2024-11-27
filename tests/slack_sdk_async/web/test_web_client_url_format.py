52927846007b6e6813857ae2ebffc8c9037a2deb
Henrique <henrique.sagara@hotmail.com>
2024-11-27 18:34:38 -0500

    test /api

--- tests/slack_sdk_async/web/test_web_client_url_format.py
+++ tests/slack_sdk_async/web/test_web_client_url_format.py
@@ -15,6 +15,8 @@ class TestAsyncWebClientUrlFormat(unittest.TestCase):
         setup_mock_web_api_server_async(self, MockHandler)
         self.client = AsyncWebClient(token="xoxb-api_test", base_url="http://localhost:8888")
         self.client_base_url_slash = AsyncWebClient(token="xoxb-api_test", base_url="http://localhost:8888/")
+        self.client_api = AsyncWebClient(token="xoxb-api_test", base_url="http://localhost:8888/api")
+        self.client_api_slash = AsyncWebClient(token="xoxb-api_test", base_url="http://localhost:8888/api/")
 
     def tearDown(self):
         cleanup_mock_web_api_server_async(self)
@@ -43,3 +45,23 @@ class TestAsyncWebClientUrlFormat(unittest.TestCase):
     async def test_base_url_without_slash_api_method_with_slash_and_trailing_slash(self):
         await self.client.api_call("/chat.postMessage/")
         await assert_received_request_count_async(self, "/chat.postMessage/", 1)
+
+    @async_test
+    async def test_base_url_with_api(self):
+        await self.client_api.api_call("chat.postMessage")
+        assert_received_request_count_async(self, "/api/chat.postMessage", 1)
+
+    @async_test
+    async def test_base_url_with_api_method_without_slash_method_with_slash(self):
+        await self.client_api.api_call("/chat.postMessage")
+        assert_received_request_count_async(self, "/api/chat.postMessage", 1)
+
+    @async_test
+    async def test_base_url_with_api_slash(self):
+        await self.client_api_slash.api_call("chat.postMessage")
+        await assert_received_request_count_async(self, "/api/chat.postMessage", 1)
+
+    @async_test
+    async def test_base_url_with_api_slash_and_method_with_slash(self):
+        await self.client_api_slash.api_call("/chat.postMessage")
+        await assert_received_request_count_async(self, "/api/chat.postMessage", 1)
