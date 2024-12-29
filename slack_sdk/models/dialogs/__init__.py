7ab1fab979be01885c9abf2adf3d38d5ef563234
William Bergamin <wbergamin@slack-corp.com>
2024-12-29 15:20:08 -0500

    Update __init__.py

--- slack_sdk/models/dialogs/__init__.py
+++ slack_sdk/models/dialogs/__init__.py
@@ -421,7 +421,7 @@ class DialogExternalSelector(AbstractDialogSelector):
 
 
 class DialogBuilder(JsonObject):
-    attributes = set()  # no attributes because to_dict has unique implementation
+    attributes = {}  # type: ignore[assignment] # no attributes because to_dict has unique implementation
 
     _callback_id: Optional[str]
     _elements: List[Union[DialogTextComponent, AbstractDialogSelector]]
