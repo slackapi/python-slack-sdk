d9b99df50ee9150e287f2fee932d7ec3c3f8bf4a
William Bergamin <wbergamin@salesforce.com>
2025-06-19 09:50:00 -0400

    chore: increase the robustness of the uninstall script (#1704)
    * chore: increase the robustness of the uninstall script
    
    * Update uninstall_all.sh

--- scripts/uninstall_all.sh
+++ scripts/uninstall_all.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-pip uninstall -y slack-sdk && \
-  pip freeze | grep -v "^-e" | sed 's/@.*//' | sed 's/\=\=.*//' | xargs pip uninstall -y
+# remove slack-sdk without a version specifier so that local builds are cleaned up
+pip uninstall -y slack-sdk
+# collect all installed packages
+PACKAGES=$(pip freeze | grep -v "^-e" | sed 's/@.*//' | sed 's/\=\=.*//')
+# uninstall packages without exiting on a failure 
+for package in $PACKAGES; do
+  pip uninstall -y $package
+done
