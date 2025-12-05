ff7a25c76f665653e283b5b964542b44375577ac
William Bergamin <wbergamin@salesforce.com>
2025-12-05 15:17:19 -0500

    Update scripts/format.sh
    Co-authored-by: Eden Zimbelman <eden.zimbelman@salesforce.com>

--- scripts/format.sh
+++ scripts/format.sh
@@ -5,6 +5,7 @@ script_dir=`dirname $0`
 cd ${script_dir}/..
 
 if [[ "$1" != "--no-install" ]]; then
+    export PIP_REQUIRE_VIRTUALENV=1
     pip install -U pip
     pip install -U -r requirements/tools.txt
 fi
