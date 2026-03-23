8b3b8cff60ade50216d6a03459a92b97ea965788
William Bergamin <wbergamin@slack-corp.com>
2026-03-23 14:16:04 -0400

    improve validation script

--- scripts/format.sh
+++ scripts/format.sh
@@ -5,7 +5,6 @@ script_dir=`dirname $0`
 cd ${script_dir}/..
 
 if [[ "$1" != "--no-install" ]]; then
-    export PIP_REQUIRE_VIRTUALENV=1
     pip install -U pip
     pip install -U -r requirements/tools.txt
 fi
--- scripts/run_validation.sh
+++ scripts/run_validation.sh
@@ -11,16 +11,20 @@ current_py=$(python --version | sed -E 's/Python ([0-9]+\.[0-9]+).*/\1/')
 
 ./scripts/install.sh
 
-echo "Generating code ..." && python scripts/codegen.py --path .
-echo "Running black (code formatter) ..." && ./scripts/format.sh --no-install
+set -e
 
-echo "Running linting checks ..." && ./scripts/lint.sh --no-install
+echo "Generating code ..." && python scripts/codegen.py --path .
+
+# Run mypy type checking only on the latest supported Python version
+if [[ "$current_py" == "$LATEST_SUPPORTED_PY" ]]; then
+    echo "Running black (code formatter) ..." && ./scripts/format.sh --no-install
+    echo "Running linting checks ..." && ./scripts/lint.sh --no-install
+    echo "Running mypy type checking ..." && ./scripts/run_mypy.sh --no-install
+else
+    echo "Skipping formatting, linting, and type checking (current Python: $current_py, required: $LATEST_SUPPORTED_PY)"
+fi
+
 
 echo "Running tests with coverage reporting ..."
 test_target="${1:-tests/}"
 PYTHONPATH=$PWD:$PYTHONPATH pytest --cov-report=xml --cov=slack_sdk/ $test_target
-
-# Run mypy type checking only on the latest supported Python version
-if [[ "$current_py" == "$LATEST_SUPPORTED_PY" ]]; then
-    echo "Running mypy type checking ..." && ./scripts/run_mypy.sh --no-install
-fi
